4ded7e05deb3f1bba49052e9fe29367d3d02df79
Luis Garbelotti <luisgsgarbelotti@gmail.com>
2021-11-21 21:45:20 -0300

    Finalizando exibição de um quizz e comportamento de resposta

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let todosQuizzes ;
 
+let respostasCorretas = 0;
+let perguntasRespondidas = 0;
+
+let conteudoQuizz;
+let arrayAlternativas = [];
+
+var cor = '';
+
+
 const  promessaObterQuizz = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
 promessaObterQuizz.then(obterQuizzes);
 
@@ -9,19 +18,23 @@ function obterQuizzes(respostaObterQuizz) {
     return respostaObterQuizz;
 }
 
+
 function imprimirQuizz(quizzes){
+
     for (let i=0; i < quizzes.length; i++) {
+
         const listaDosQuizzes = document.querySelector(".lista-quizz-recebido");
         listaDosQuizzes.innerHTML += `<li id="${quizzes[i].id}" class="quizz ponteiro" onclick="obterUnicoQuizz(this)"> 
                                             
                                             <img src="${quizzes[i].image}">
                                             <div class="degrade">
                                                     <div>${quizzes[i].title}</div>
-                                            </div>
-                                            
+                                            </div>  
                                         </li>`;
+
     }
 }
+
 
 function obterUnicoQuizz(quizzSelecionado) {
 
@@ -36,64 +49,134 @@ function obterUnicoQuizz(quizzSelecionado) {
 
 }
 
-let conteudoQuizz;
+
+function comparador() { 
+
+	return Math.random() - 0.5; 
+
+}
+
 
 function mostrarUnicoQuizz(respostaUnicoQuizz) {
     
     conteudoQuizz = respostaUnicoQuizz.data;
-    
+    let respostas;
     const unicoQuizz = document.querySelector(".pagina-quizz");
     unicoQuizz.innerHTML = `
                                 <img class="imagem-quizz" src="${conteudoQuizz.image}">
                                     <div class="opacidade"> 
                                         <div class="display-flex">${conteudoQuizz.title}</div>
-                                    </div>`
+                                    </div>`;
                                     
-    for(i=0;i<conteudoQuizz.questions.length;i++){
+    for( i = 0; i < conteudoQuizz.questions.length; i++){
+
+        arrayAlternativas = [];
+        
         unicoQuizz.innerHTML +=                       `
-                                <div  class="perguntas">
-                                    <div id="0${i }" class="texto-pergunta display-flex">${conteudoQuizz.questions[i].title}</div>
-                                    <ul class="todas-alternativas${i}">
+                                <div id="9${i}" class="perguntas">
+                                    <div id="${i }" class="texto-pergunta display-flex">${conteudoQuizz.questions[i].title}</div>
+                                    <ul class="todas-alternativas${i} lista">
+                                    
                                     </ul>`;
 
         corAleatoria();
 
-        document.getElementById(0+[i]).style.backgroundColor = color;
+        document.getElementById([i]).style.backgroundColor = cor;
                                     
-        for(j=0;j<conteudoQuizz.questions[i].answers.length;j++) {
-            const respostas = document.querySelector(".todas-alternativas" + i);
+        for(j = 0; j < conteudoQuizz.questions[i].answers.length; j++) {
 
-            respostas.innerHTML +=    `
-                                        <li id="${j}" class="alternativa ponteiro" onclick="selecionaResposta(this)">
+            respostas = document.querySelector(".todas-alternativas" + i);
+            let variavelAuxiliar = `
+                                        <li id="${i}${j}" class="alternativa ponteiro " onclick="selecionaResposta(this)">
                                             <img src="${conteudoQuizz.questions[i].answers[j].image}" >
-                                             <p> ${conteudoQuizz.questions[i].answers[j].text} </p>
+                                            <p> ${conteudoQuizz.questions[i].answers[j].text} </p>
+                                            <p class=""> ${conteudoQuizz.questions[i].answers[j].isCorrectAnswer} </p>
                                         </li>
                                 `;
+            arrayAlternativas.push(`${variavelAuxiliar}`);
+
         }
-            
+        
+        arrayAlternativas.sort(comparador);
+
+        for( let k = 0 ; k < arrayAlternativas.length; k++){
+
+            respostas.innerHTML += `${arrayAlternativas[k]}`;
+        
+        }  
     }
 }
 
 
-var color = '';
 function corAleatoria() {
 
-    var letters = '0123456789ABCDEF';
-    color = '#';
+    var letras = '0123456789ABCDEF';
+    cor = '#';
 
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        cor += letras[Math.floor(Math.random() * 16)];
     }
-    
-    return color;
+    return cor;
 
 }
 
 
 function selecionaResposta ( respostaEscolhida ) {
- 
-    console.log(respostaEscolhida);
- 
+
+    const elementoRespostas = respostaEscolhida.parentNode;
+
+    for(let i = 0; i <conteudoQuizz.questions.length; i++){
+
+        if ( elementoRespostas.classList.contains('todas-alternativas' + i) ){
+
+            const erradas = document.querySelectorAll(".todas-alternativas" + i +" li.alternativa");
+                
+            for(let j = 0; j < erradas.length; j++){
+
+                    erradas[j].classList.add('esbranquicado');
+                    erradas[j].classList.add('errada');
+                    erradas[j].removeAttribute("onclick");
+
+                    if (conteudoQuizz.questions[i].answers[j].isCorrectAnswer === true ){
+                        
+                        const respostaCerta = document.getElementById(`${i}` + `${j}`);
+                        respostaCerta.classList.remove('errada');
+                        respostaCerta.classList.add('certa');
+
+                        if( respostaEscolhida === respostaCerta ) {
+                            respostasCorretas++;
+                        }
+                    }
+            }
+
+        respostaEscolhida.classList.remove('esbranquicado');
+        
+        } 
+    }
+
+    perguntasRespondidas++;
+    setTimeout(scrollProximaPergunta, 2000);
+
 }
         
+
+function scrollProximaPergunta(){
+        
+    if ( perguntasRespondidas === conteudoQuizz.questions.length) {
+
+        finalizarQuizz();
+
+    } else {
+
+        const irParaProxima = document.getElementById(`9${perguntasRespondidas}`);
+        irParaProxima.scrollIntoView({behavior: "smooth"});
+
+    }
+}
+
+
+function finalizarQuizz() {
+
+    
+}
 
